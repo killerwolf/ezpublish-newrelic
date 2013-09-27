a6118fda4770e4f1496ade0fcdcecd226b4347cb
Hamdi LAADHARI <killerwolf06@gmail.com>
2013-09-27 08:52:56 +0200

    first draft

---
+++ classes/newrelichandler.php
@@ -0,0 +1,31 @@
+<?php
+
+use Intouch\Newrelic\Newrelic;
+
+class newRelicHandler{
+	static public function trackTransaction(){
+		eZExecution::addCleanupHandler(function(){
+			$newrelic = new Newrelic( true );
+			$newrelic->nameTransaction( self::buildCurrentTransactionName() );
+		});
+	}
+
+	static function buildCurrentTransactionName(){
+		$trans = array();
+		if(isset($GLOBALS['eZRequestedModuleParams']))
+        {
+            if(isset($GLOBALS['eZRequestedModuleParams']['module_name']))
+            {
+                $trans[] = $GLOBALS['eZRequestedModuleParams']['module_name'];
+                if(isset($GLOBALS['eZRequestedModuleParams']['function_name']))
+                {
+                    $trans[] = $GLOBALS['eZRequestedModuleParams']['function_name'];
+                }
+            }
+        }
+
+        return (count($trans)? implode($trans,'|'):'Others');
+	}
+}
+
+?>
