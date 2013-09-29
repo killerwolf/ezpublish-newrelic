454ff219ca8f6c59f587efe75850c4f5d57ff0bf
Hamdi LAADHARI <killerwolf06@gmail.com>
2013-09-29 14:58:40 +0200

    removing useless comments

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -1,10 +1,5 @@
 <?php /*
 
 [Event]
-# (This is private api and might change in future versions)
-# List of global event listeners/filters in the form:
-#Listeners[]=<event>@<callback>
-# eg:
-#Listeners[]=request/input@ezxFormHandler::input
 Listeners[]=request/input@newRelicHandler::trackTransaction
 */ ?>
